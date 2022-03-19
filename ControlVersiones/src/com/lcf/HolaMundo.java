d96504b644d07e6f435887ab30115a21507d88a7
LuisCastro1983 <101797944+LuisCastro1983@users.noreply.github.com>
2022-03-19 02:56:37 +0100

    Escribimos comentario

--- ControlVersiones/src/com/lcf/HolaMundo.java
+++ ControlVersiones/src/com/lcf/HolaMundo.java
@@ -5,6 +5,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola");
 		System.out.println("Mundo");
+		//Editando el código desde repositorio remoto
 
 	}
 
